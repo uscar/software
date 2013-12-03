8bf77518858b5056f7dff299c586efc712548a77
Nolan Miller <namiller@usc.edu>
2013-12-02 21:20:48 -0800

    updated to latest

--- APM2/write_testing/write_testing.ino
+++ APM2/write_testing/write_testing.ino
@@ -10,26 +10,48 @@
 #include <AP_HAL.h>
 #include <AP_HAL_AVR.h>
 
+#include <AP_InertialSensor.h>
+#include <GCS_MAVLink.h>
 
+#include <AP_Notify.h>
+#include "uscar_printing.h";
 
 const AP_HAL::HAL& hal = AP_HAL_AVR_APM2;
+AP_InertialSensor_MPU6000 ins;
 
-AP_ADC_HIL adc;
 
 void setup(void)
 {
     // HAL will start serial port at 115200.
-    hal.console->println_P(PSTR("Starting!"));
-    adc.Init();
+  hal.console->println_P(PSTR("Starting!"));
+  hal.gpio->pinMode(40, GPIO_OUTPUT);
+  hal.gpio->write(40,1);
+  ins.init(AP_InertialSensor::COLD_START,AP_InertialSensor::RATE_100HZ);
 }
 
 void loop(void)
 {
-  hal.scheduler->delay(100);
-  for(int i = 0;i<8;i++){
-    hal.console->printf(" %f ",adc.Ch(i));
-  }
+  hal.scheduler->delay(10);
+  printIMU();
+}
+
+void printIMU(){
+  Vector3f acc;
+  float acc_amp;
+  Vector3f gyr;
+  
+  ins.update();
+  acc = ins.get_accel();
+  gyr = ins.get_gyro();
+  acc_amp = acc.length();
+  char str[100];
+  hal.console->printf("acc:");
+  printv3f(acc,hal);
+  hal.console->printf("  gyr:");
+  printv3f(gyr,hal);
   hal.console->printf("\n");
 }
+
+
 
 AP_HAL_MAIN();
