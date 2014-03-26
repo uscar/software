f8016f60ac07788822dd0f37a1c13970ae46fe45
Nolan Miller <namiller@usc.edu>
2014-03-26 11:48:16 -0700

    added gyr stability

--- APM_Arduino/bare_flight/bare_flight.ino
+++ APM_Arduino/bare_flight/bare_flight.ino
@@ -12,100 +12,110 @@ AP_Motors:
  *******************************************************************************/
 
 
-
-#include <PID.h>
-
+//General board dependencies
 #include <AP_ADC_HIL.h>
 #include <AP_ADC.h>
-
 #include <AP_Common.h>
 #include <AP_Math.h>
 #include <AP_Param.h>
 #include <AP_Progmem.h>
-
 #include <AP_HAL.h>
 #include <AP_HAL_AVR.h>
 
+//IMU sensor dependencies
 #include <AP_InertialSensor.h>
 #include <GCS_MAVLink.h>
+#include <AP_Notify.h>
 
-#include <RC_Channel.h>     // RC Channel Library
+//Motor output dependencies
+#include <RC_Channel.h> 
 #include <AP_Motors.h>
 #include <AP_Curve.h>
 
-#include <Filter.h>
+//Algorithm tools
 #include <LowPassFilter2p.h>
-
-#include <AP_Notify.h>
-
-#include <PID.h> //pid controller
 #include <AC_PID.h>
 
+//Custom tools
 #include "calibration.h"
 #include "printing.h"
 
-
+//board MUST be declared like this for libraries to function (they use extern)
 const AP_HAL::HAL& hal = AP_HAL_AVR_APM2;
+
 AP_InertialSensor_MPU6000 ins;
 
-//setupt the filters (for getting down)
+//setup the filters for removing noise from acc.
 static LowPassFilter2p filt_x(update_hz,cutout);
 static LowPassFilter2p filt_y(update_hz,cutout);
 static LowPassFilter2p filt_z(update_hz,cutout);
 
-//this may need to be inside loop for some reason...
-AC_PID pid_roll(    r_p,r_i,r_d,r_imax);
-AC_PID pid_pitch(   p_p,p_i,p_d,p_imax);
-AC_PID pid_throttle(t_p,t_i,t_d,t_imax);
-AC_PID pid_yaw(     y_p,y_i,y_d,y_imax);
+//set up the PID calculators with the hard coded values
+AC_PID pid_roll     (r_p,r_i,r_d,r_imax);
+AC_PID pid_pitch    (p_p,p_i,p_d,p_imax);
+AC_PID pid_throttle (t_p,t_i,t_d,t_imax);
+AC_PID pid_yaw      (y_p,y_i,y_d,y_imax);
 
-//rc channel is a useful data type for defining a command.
-//not restricted to remote control inputs. Good for computer defined too.
 //needed by the AP_Motors type to define inputs
 RC_Channel m_roll(2), m_pitch(3), m_throttle(1), m_yaw(4);
-RC_Channel c_roll(12), c_pitch(13), c_throttle(13), c_yaw(14);
+
+//vector command storage
+Vector3f cntrl_up;
+Vector3f old_cntrl_up;
+
+//non-vector command storage
+float cntrl_yaw;
+int cntrl_throttle;
 
 //motors abstractions handles the mapping of command to motor output
-
 //CAREFULL -> the motors use the pwm in value not the mapped value.
 AP_MotorsQuad motors(&m_roll,&m_pitch,&m_throttle,&m_yaw);
 
+//used for calculating update rate, time elapsed etc
 int timestamp;
 int t0;
 int counter = 0;
 
-int tempy = 0;
-
-//sets the initial values as desired (deprecated)
-void init_rc(RC_Channel* rc){
-    rc->set_range((int16_t)CNTRL_MIN,(int16_t)CNTRL_MAX);
-    rc->radio_min = CNTRL_MIN;
-    rc->radio_max = CNTRL_MAX;
-    rc->set_pwm((int16_t)CNTRL_MIN);
-    rc->trim();
-    rc->set_pwm((int16_t)CNTRL_MIN);
-}
 
 //reads remote control inputs and sets c vals
 void read_rc_inputs(){
-//    tempy++;
-//    tempy%=1000;
-//    int val = sin(tempy*PI*2/1000)*400+1500;
-//    c_roll.set_pwm(1500);
-//    c_pitch.set_pwm(1500);
-//    c_throttle.set_pwm(val);
-//    c_yaw.set_pwm(1500);
+
+    int roll        = hal.rcin->read(CH_1);
+    int pitch       = hal.rcin->read(CH_2);
+    int throttle    = hal.rcin->read(CH_3);
+    int yaw         = hal.rcin->read(CH_4);
     
-    c_roll.set_pwm(hal.rcin->read(CH_1));
-    c_pitch.set_pwm(hal.rcin->read(CH_2));
-    c_throttle.set_pwm(hal.rcin->read(CH_3));
-    c_yaw.set_pwm(hal.rcin->read(CH_4));    
+    float range = REMOTE_MAX-REMOTE_MIN;
+
+    //calculate control value from input value
+    cntrl_up.x = (INV_PITCH?-1:1)*(pitch - REMOTE_TRIM)*2.0/range;
+    cntrl_up.y = (INV_ROLL?-1:1)*(roll - REMOTE_TRIM)*2.0/range;
+    cntrl_up.z = CNTRL_Z_COMP; //dampens the control.
     
+    cntrl_up = cntrl_up.normalized();
+    cntrl_yaw  = (INV_YAW?-1:1)*(yaw - REMOTE_TRIM)*2.0/range;
+    cntrl_throttle = (throttle - REMOTE_MIN);
 }   
 
 //linearly maps a value within one range to another range
 int lin_map(int value, int min_v, int max_v, int min_o, int max_o){
     return (int)((value-min_v)*((float)(max_o-min_o)/(float)(max_v-min_v))+min_o);
+}
+
+void setup_m_rc(){
+
+//setting to less than 1000 scales up control effect
+    m_throttle.set_range(0,750 );//should be 1000 probs.
+    m_roll.set_range    (0,1000);
+    m_pitch.set_range   (0,1000);
+    m_yaw.set_range     (0,1000);
+    
+    m_throttle.radio_min = 1000;
+    m_throttle.radio_max = 2000;
+
+    m_roll.servo_out = 0;
+    m_pitch.servo_out = 0;
+    m_yaw.servo_out = 0;
 }
 
 //called once on reset
@@ -118,71 +128,38 @@ void setup(void)
 
     //defines the mapping system from RC command to motor effect (defined in quad)
     motors.set_frame_orientation(AP_MOTORS_X_FRAME);//motors.set_frame_orientation(AP_MOTORS_H_FRAME);
-    //the times per second to update the outpus (from the rc vals)
-    motors.set_min_throttle((uint16_t) 130);
-    motors.slow_start(false);
-    //motors.set_max_throttle((uint16_t) MOTOR_MAX);
-    //motors.set_mid_throttle((uint16_t) MOTOR_HOVER);
+    //motors.min_throttle is in terms of servo values not output values.
+
+    //setup rc
+    setup_m_rc();
+
+    //setup motors
     motors.Init();
     motors.set_update_rate(500);
-
     motors.enable();
-    motors.output_min();
-
     motors.armed(true);
 
-
+    //setup timing
     t0 = timestamp = hal.scheduler->micros();
 
-    init_rc(&m_roll);     init_rc(&c_roll);   
-    init_rc(&m_pitch);    init_rc(&c_pitch);  
-    init_rc(&m_throttle); init_rc(&c_throttle);
-    init_rc(&m_yaw);      init_rc(&c_yaw);
-
-    //for reading from the actual remote control input
-    //radio_min/max are the input range
-    c_roll.radio_min = c_pitch.radio_min = c_throttle.radio_min = c_yaw.radio_min = REMOTE_MIN;
-    c_roll.radio_max = c_pitch.radio_max = c_throttle.radio_max = c_yaw.radio_max = REMOTE_MAX;
-    c_roll.radio_trim = c_pitch.radio_trim = c_yaw.radio_trim = REMOTE_TRIM; //don't trim the throttle.
-    c_throttle.radio_trim = REMOTE_MIN;
-    //this min/max defines the output range
-    c_roll.set_range(CNTRL_MIN,CNTRL_MAX);
-    c_throttle.set_range(CNTRL_MIN,CNTRL_MAX);
-    c_yaw.set_range(CNTRL_MIN,CNTRL_MAX);
-    c_pitch.set_range(CNTRL_MIN,CNTRL_MAX);
-
-
-    m_roll.radio_min = m_pitch.radio_min = m_throttle.radio_min = m_yaw.radio_min = MOTOR_MIN;
-    m_roll.radio_max = m_pitch.radio_max = m_throttle.radio_max = m_yaw.radio_max = MOTOR_MAX;
-    /*
-       m_roll.set_range(MOTOR_MIN,MOTOR_MAX);
-       m_pitch.set_range(MOTOR_MIN,MOTOR_MAX);
-       m_yaw.set_range(MOTOR_MIN,MOTOR_MAX);
-       m_throttle.set_range(MOTOR_MIN,MOTOR_MAX);
-     */
-    //kills the barometer
+    //kill the barometer
     hal.gpio->pinMode(40, GPIO_OUTPUT);
     hal.gpio->write(40,1);
 
+    ///not the right place but it is a nice thought    
+    old_cntrl_up = cntrl_up;
 }
 
 //called itteratively
-void loop(void)
-{
+void loop(void){
     //for periodic outputs
     counter++;
-    counter %= 10;
+    counter %= 50;
 
     //update the time locks/ time updates
     int t = hal.scheduler->micros();
     float dt = (t - timestamp)/1000.0;
     timestamp = t;
-
-    if(DEBUG_TIMER || DEBUG_ALL){
-        hal.console->printf("%f",dt);
-        if(counter == 0 )
-            hal.console->printf("\n");
-    }
 
 
     //get new instrument measurement
@@ -200,67 +177,103 @@ void loop(void)
     filtered_acc.y = filt_y.apply(acc.y);
     filtered_acc.z = filt_z.apply(acc.z);
 
+    //turn off the filtering
+    //    filtered_acc.x = acc.x;
+    //    filtered_acc.y = acc.y;
+    //    filtered_acc.z = acc.z;
+
+
     //normalize (length = 1)
-    Vector3f down = filtered_acc.normalized();
+    Vector3f down = filtered_acc.normalized(); // may break in free fall...
 
     if(DEBUG_FILTER || DEBUG_ALL){
         if(counter == 0)
             hal.console->printf("down = %3.3f %3.3f %3.3f\n",down.x,down.y,down.z);
     }
 
-    //take appropriate componenets and scale
-    int roll_actual = down.y*(-1)*(CNTRL_MAX-CNTRL_MIN)+(CNTRL_MAX+CNTRL_MIN)/2;
-    int pitch_actual = down.x*(CNTRL_MAX-CNTRL_MIN)+(CNTRL_MAX+CNTRL_MIN)/2;
-    int throttle_actual = c_throttle.control_in; //TODO: change this out with some f(height)
-    int yaw_actual = gyr.z*(CNTRL_MAX-CNTRL_MIN)+(CNTRL_MAX+CNTRL_MIN)/2;                  //this may require some scaling
+    Vector3f err = down + cntrl_up;
 
-    if(DEBUG_ACTUAL || DEBUG_ALL){
-        if(counter == 0)
-            hal.console->printf("actual roll= %04i, actual pitch= %04i, actual throttle= %04i, actual yaw= %04i\n",roll_actual,pitch_actual,throttle_actual,yaw_actual);
-    }
+    //make response smaller near center
+    //    err.x = (1-cos(err.x*PI))*(abs(err.x)/err.x)*.1+err.x*.9;
+    //    err.y = (1-cos(err.y*PI))*(abs(err.y)/err.y)*.1+err.y*.9;
+
+
+
+
+
+    //take appropriate componenets and scale
+    int roll_actual = (-1)*down.y*CNTRL_RANGE;
+    int pitch_actual = down.x*CNTRL_RANGE;
+    int throttle_actual = cntrl_throttle; //TODO: change this out with some f(height)
+    int yaw_actual = gyr.z*YAW_SCALE;                  //this may require some scaling
+
 
     read_rc_inputs();
 
+    Vector3f d_cntrl = (cntrl_up - old_cntrl_up)/dt;
+    old_cntrl_up = cntrl_up;
+
+    Vector3f gyr_err = (gyr - d_cntrl)*GYR_ERR_SCALE;
+
+    //    if(counter==0){
+    //      printv3f(d_cntrl);
+    //      printv3f(gyr);
+    //      printv3f(gyr_err);
+    //    }
 
 
-    int roll_error     =  c_roll.control_in     - roll_actual;
-    int pitch_error    =  c_pitch.control_in    - pitch_actual;
-    int throttle_error =  c_throttle.control_in - throttle_actual;
-    int yaw_error      =  c_yaw.control_in      - yaw_actual;
+    int int_cntrl_roll      = cntrl_up.y*CNTRL_RANGE;
+    int int_cntrl_pitch     = cntrl_up.x*CNTRL_RANGE;
+    int int_cntrl_yaw       = cntrl_yaw*CNTRL_RANGE;
+    int int_cntrl_throttle  = cntrl_throttle;
 
-    int r_correction = error_scale*pid_roll.get_pid(roll_error,dt);
-    int p_correction = error_scale*pid_pitch.get_pid(pitch_error,dt);
+    int roll_error     =  err.y*CNTRL_RANGE ;
+    int pitch_error    =  err.x*CNTRL_RANGE ;
+    int throttle_error =  cntrl_throttle - throttle_actual;
+    int yaw_error      =  int_cntrl_yaw - yaw_actual;
+
+    int r_correction = error_scale*(pid_roll.get_pid(roll_error,dt)- (int)gyr_err.x);
+    int p_correction = error_scale*(pid_pitch.get_pid(pitch_error,dt)+ (int)gyr_err.y);
     int t_correction = error_scale*pid_throttle.get_pid(throttle_error,dt);
     int y_correction = error_scale*pid_yaw.get_pid(yaw_error,dt);
 
+
+    if(DEBUG_ACTUAL || DEBUG_ALL){
+        if(counter == 0)
+            hal.console->printf("actual: roll= %04i, pitch= %04i, throttle= %04i, yaw= %04i\n",roll_actual,pitch_actual,throttle_actual,yaw_actual);
+    }
+
+    if(DEBUG_CONTROL || DEBUG_ALL){
+        if(counter == 0){
+            hal.console->printf("cntrl: roll= %04i, pitch= %04i, throttle= %04i, yaw= %04i\n",int_cntrl_roll, int_cntrl_pitch, int_cntrl_throttle,int_cntrl_yaw); 
+        }
+    }
+
     if(DEBUG_PID || DEBUG_ALL){
         if(counter == 0)
-            hal.console->printf("roll correction= %04i, pitch correction= %04i, throttle correction= %04i, yaw correction= %04i\n",r_correction,p_correction,t_correction,y_correction);
+            hal.console->printf("correction: roll = %04i, pitch = %04i, throttle = %04i, yaw = %04i\n",r_correction,p_correction,t_correction,y_correction);
     }
-    int roll_out = c_roll.control_in + r_correction;
-    int pitch_out = c_pitch.control_in + p_correction;
-    int throttle_out = c_throttle.control_in + p_correction;
-    int yaw_out = c_yaw.control_in + y_correction;
-    roll_out = max(roll_out, CNTRL_MIN);
-    pitch_out = max(pitch_out, CNTRL_MIN);
-    throttle_out = max(throttle_out, CNTRL_MIN);
-    yaw_out = max(yaw_out, CNTRL_MIN);
 
-    roll_out = min(roll_out, CNTRL_MAX);
-    pitch_out = min(pitch_out, CNTRL_MAX);
-    throttle_out = min(throttle_out, CNTRL_MAX);
-    yaw_out = min(yaw_out, CNTRL_MAX);
+    int roll_out = int_cntrl_roll + r_correction;
+    int pitch_out = int_cntrl_pitch + p_correction;
+    int throttle_out = int_cntrl_throttle + t_correction;
+    int yaw_out = int_cntrl_yaw + y_correction;
+
+    if(DEBUG_CNTRL || DEBUG_ALL){
+        if(counter == 0)
+            hal.console->printf("out: roll = %04i, pitch = %04i, throttle = %04i, yaw = %04i\n",roll_out,pitch_out,throttle_out,yaw_out);
+    }
 
     //adjust motor outputs approrpiately for the pid term
-    int roll_pulse = lin_map(roll_out, CNTRL_MIN, CNTRL_MAX, MOTOR_MIN, MOTOR_MAX);
-    int pitch_pulse = lin_map(pitch_out, CNTRL_MIN, CNTRL_MAX, MOTOR_MIN, MOTOR_MAX);
-    int throttle_pulse = lin_map(throttle_out, CNTRL_MIN, CNTRL_MAX, MOTOR_MIN, MOTOR_MAX);
-    int yaw_pulse = lin_map(yaw_out, CNTRL_MIN, CNTRL_MAX, MOTOR_MIN, MOTOR_MAX);
+    int roll_pulse = roll_out;
+    int pitch_pulse = pitch_out;
+    int throttle_pulse = throttle_out;
+    int yaw_pulse = yaw_out;
 
-    m_roll.set_pwm(roll_pulse);
-    m_pitch.set_pwm(pitch_pulse);
-    m_throttle.set_pwm(throttle_pulse);
-    m_yaw.set_pwm(yaw_pulse);
+    m_roll.servo_out = roll_pulse;
+    m_pitch.servo_out = pitch_pulse;
+    m_throttle.servo_out = throttle_pulse;
+    m_yaw.servo_out = yaw_pulse;
 
     if(DEBUG_PULSE || DEBUG_ALL){
         if(counter == 0){
@@ -271,15 +284,12 @@ void loop(void)
     motors.output();
 
 
-    if(DEBUG_CONTROL || DEBUG_ALL){
-        if(counter == 0){
-            hal.console->printf("rcout: roll= %04i, pitch= %04i, throttle= %04i, yaw= %04i\n",c_roll.control_in, c_pitch.control_in, c_throttle.control_in,c_yaw.control_in); 
-            hal.console->printf("rcin: roll= %04i, pitch= %04i, throttle= %04i, yaw= %04i\n",c_roll.radio_in, c_pitch.radio_in, c_throttle.radio_in,c_yaw.radio_in);   
-        }
-    }
+
     if(DEBUG_OUTPUT|| DEBUG_ALL){
-        if(counter == 0)
-            hal.console->printf("output roll= %04i, pitch= %04i, throttle= %04i, yaw= %04i\n",m_roll.radio_in, m_pitch.radio_in, m_throttle.radio_in,m_yaw.radio_in); 
+        if(counter == 0){
+            hal.console->printf("servo output roll= %04i, pitch= %04i, throttle= %04i, yaw= %04i\n",m_roll.servo_out, m_pitch.servo_out, m_throttle.servo_out,m_yaw.servo_out); 
+            hal.console->printf("pwm   output roll= %04i, pitch= %04i, throttle= %04i, yaw= %04i\n",m_roll.pwm_out, m_pitch.pwm_out, m_throttle.pwm_out,m_yaw.pwm_out);    
+        }
     }
     if(DEBUG_MOTOR || DEBUG_ALL){
         if(counter == 0)
@@ -293,6 +303,8 @@ void loop(void)
         if(counter == 0)
             hal.console->printf("pwm roll:%i pitch:%i throttle:%i yaw:%i\n",(int)m_roll.servo_out, (int)m_pitch.servo_out, (int)m_throttle.servo_out, (int)m_yaw.servo_out);
     }
+
+
     //TODO:provide user interface for calibrating the accelerometer
     //also same thing for pid stuff
 }
--- APM_Arduino/bare_flight/calibration.h
+++ APM_Arduino/bare_flight/calibration.h
@@ -24,15 +24,15 @@ float scale_gyr[3]   = {1,1,1};
 
 //PID values
 
-#define r_p      1.0
-#define r_i      0.03
-#define r_d      0.2
-#define r_imax   20
+#define r_p      2.0
+#define r_i      0.05
+#define r_d      0.3
+#define r_imax   5
 
-#define p_p     1.0
-#define p_i     0.03
-#define p_d     0.2
-#define p_imax  20
+#define p_p     2.0
+#define p_i     0.05
+#define p_d     0.3
+#define p_imax  5
 
 #define t_p    1.0
 #define t_i    0.001
@@ -44,32 +44,59 @@ float scale_gyr[3]   = {1,1,1};
 #define y_d          .2
 #define y_imax       10
 
-#define error_scale .4
+#define error_scale .3
 
 //low pass filter
 #define update_hz 1000
 #define cutout 40
 
 
+#define GYR_ERR_SCALE 200
+
+#define INV_ROLL false
+#define INV_YAW true
+#define INV_PITCH true
+
 
 //debugging setup
-#define DEBUG_ALL false
+#define DEBUG_ALL true
 #define DEBUG_FILTER false
-#define DEBUG_PID false
+#define DEBUG_PID true
 #define DEBUG_ACTUAL true
-#define DEBUG_MOTOR true
+#define DEBUG_MOTOR false
 #define DEBUG_TIMER false
-#define DEBUG_OUTPUT true
+#define DEBUG_OUTPUT false
 #define DEBUG_PWM false
 #define DEBUG_CONTROL false
 #define DEBUG_ENABLE false
-#define DEBUG_PULSE true
+#define DEBUG_PULSE false
+#define DEBUG_CNTRL false
 //random
 
-#define CNTRL_MIN 0
-#define CNTRL_MAX 1000
 
 //the min/max signals to which the output to motors are constrained
+#define M_SERV_MIN 0
+#define M_SERV_MAX 1000
+
+#define THROTTLE_SIG_MAX 1000
+#define THROTTLE_SIG_MIN 0
+
+#define DIR_SIG_MIN -1000
+#define DIR_SIG_MAX 1000
+
 #define MOTOR_MIN 1150
 #define MOTOR_MAX 1900
 #define MOTOR_HOVER 1400
+
+//how much is yaw rate weighted relative to acc.
+#define YAW_SCALE 100
+
+
+#define RPY_RC_MIN 1100
+#define RPY_RC_MAX 1900
+#define RPY_RC_TRIM 1500
+
+//the dynamic range given (zeor to max, min to zero) for cntrl values
+#define CNTRL_RANGE 1000
+//the z componenet of the to-be normalized control vector (a large value dampens controller effect)
+#define CNTRL_Z_COMP 5
