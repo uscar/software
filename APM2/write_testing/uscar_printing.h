c5deb175c34e0663b68576ee970700fe4c0f6d10
Nolan Miller <namiller@usc.edu>
2014-04-17 15:18:50 -0700

    removed unnessesary folder

--- APM2/write_testing/uscar_printing.h
+++
@@ -1,9 +0,0 @@
-
-//<---------declarations-------->
-void printv3f(Vector3f & v, const AP_HAL::HAL & h);
-
-//<-------implementations------->
-void printv3f(Vector3f & v, const AP_HAL::HAL & h){
-  h.console->printf("<%f,%f,%f>",v.x,v.y,v.z); 
-}
-
