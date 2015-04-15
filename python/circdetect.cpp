fc09bb1511df1da38b9cf8896c54ecb8a67eb6b5
DarvishKamalia <darvish.kamalia@gmail.com>
2015-04-14 21:34:42 -0700

    Changed SURF to FAST

--- python/circdetect.cpp
+++ python/circdetect.cpp
@@ -10,6 +10,7 @@
 #include "opencv2/features2d/features2d.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/nonfree/features2d.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 using namespace cv;
 using namespace std;
@@ -36,8 +37,9 @@ using namespace std;
 int main( int argc, char** argv )
 {
 
-  VideoCapture cap(0);
-  Mat img_1 = imread( "roomba.jpg", CV_LOAD_IMAGE_GRAYSCALE );
+  CvCapture* cap = cvCaptureFromCAM(CV_CAP_ANY);
+
+  Mat img_1 = imread( "roombatest.jpg" );
 
   if( !img_1.data )
   { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
@@ -45,26 +47,25 @@ int main( int argc, char** argv )
   //-- Step 1: Detect the keypoints using SURF Detector
   int minHessian = 400;
 
-  SurfFeatureDetector detector( minHessian );
+  FastFeatureDetector detector( minHessian );
 
   std::vector<KeyPoint> keypoints_1, keypoints_2;
 
-  detector.detect( img_1, keypoints_1 );
+  //detector.detect( img_1, keypoints_1 );
 
   //-- Step 2: Calculate descriptors (feature vectors)
   SurfDescriptorExtractor extractor;
 
   Mat descriptors_1, descriptors_2;
-
+   extractor.compute( img_1, keypoints_1, descriptors_1 );
 
   while (true) {
 
   Mat img_2;
 
-  cap >> img_2; 
+  img_2 = cvQueryFrame(cap);
 
   detector.detect( img_2, keypoints_2 );
-  extractor.compute( img_1, keypoints_1, descriptors_1 );
   extractor.compute( img_2, keypoints_2, descriptors_2 );
 
   //-- Step 3: Matching descriptor vectors using FLANN matcher
@@ -80,9 +81,6 @@ int main( int argc, char** argv )
     if( dist < min_dist ) min_dist = dist;
     if( dist > max_dist ) max_dist = dist;
   }
-
-  printf("-- Max dist : %f \n", max_dist );
-  printf("-- Min dist : %f \n", min_dist );
 
   //-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
   //-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
@@ -101,14 +99,19 @@ int main( int argc, char** argv )
                good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
                vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
 
-    //-- Show detected matches
-    imshow( "Good Matches", img_matches );
+  //-- Show detected matches
+  imshow( "Roomba Detection", img_matches);
 
-  for( int i = 0; i < (int)good_matches.size(); i++ )
-  { printf( "-- Good Match [%d] Keypoint 1: %d  -- Keypoint 2: %d  \n",
-            i, good_matches[i].queryIdx, good_matches[i].trainIdx ); }
+  for( int i = 0; i < (int)good_matches.size(); i++ ) {
 
+      cout << "found match " << endl; 
+  }
 
   }
+
+  waitKey(0);
+
+
+
 
 }
