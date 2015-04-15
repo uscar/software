deba0cb6a315cf05b2a9262cd2564f6e0f7ab933
Nolan Miller <namiller@usc.edu>
2015-04-14 21:49:04 -0700

    reformatted to make nicer/slightly faster

--- python/circdetect.cpp
+++ python/circdetect.cpp
@@ -15,103 +15,76 @@
 using namespace cv;
 using namespace std;
 
-// int main () {
-//
-//   VideoCapture cap(0);
-//   Mat src = imread("roomba.jpg");
-//   resize(src, src, Size_<int>(600, 600));
-//   SurfFeatureDetector surf(40);
-//   vector<KeyPoint> ks1;
-//   Mat ds1;
-//   surf(src, cv::noArray(),
-//     ks1,  ds1);
-//
-//
-//
-// }
-
-/**
- * @function main
- * @brief Main function
- */
 int main( int argc, char** argv )
 {
+    CvCapture* cap = cvCaptureFromCAM(CV_CAP_ANY);
+    Mat img_1 = imread( "roombatest.jpg" );
+    if( !img_1.data )
+    { 
+        cout<< " --(!) Error reading images " << endl; 
+        return -1; 
+    }
 
-  CvCapture* cap = cvCaptureFromCAM(CV_CAP_ANY);
+    //-- Step 1: Detect the keypoints using SURF Detector
+    const int kMinHessian = 400;
+    SurfFeatureDetector detector( kMinHessian );
+    vector<KeyPoint> keypoints_1, keypoints_2;
 
-  Mat img_1 = imread( "roombatest.jpg" );
+    //-- Step 2: Calculate descriptors (feature vectors)
+    SurfDescriptorExtractor extractor;
 
-  if( !img_1.data )
-  { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
+    Mat descriptors_1, descriptors_2;
+    extractor.compute( img_1, keypoints_1, descriptors_1 );
 
-  //-- Step 1: Detect the keypoints using SURF Detector
-  int minHessian = 400;
+    while (true) {
+        Mat img_2 = cvQueryFrame(cap);
 
-  FastFeatureDetector detector( minHessian );
+        detector.detect( img_2, keypoints_2 );
+        extractor.compute( img_2, keypoints_2, descriptors_2 );
 
-  std::vector<KeyPoint> keypoints_1, keypoints_2;
+        //-- Step 3: Matching descriptor vectors using FLANN matcher
 
-  //detector.detect( img_1, keypoints_1 );
+        FlannBasedMatcher matcher;
+        std::vector< DMatch > matches;
+        matcher.match( descriptors_1, descriptors_2, matches );
 
-  //-- Step 2: Calculate descriptors (feature vectors)
-  SurfDescriptorExtractor extractor;
+        double max_dist = 0; double min_dist = 100;
 
-  Mat descriptors_1, descriptors_2;
-   extractor.compute( img_1, keypoints_1, descriptors_1 );
+        //-- Quick calculation of max and min distances between keypoints
+        for( int i = 0; i < descriptors_1.rows; i++ )
+        { double dist = matches[i].distance;
+            if( dist < min_dist ) min_dist = dist;
+            if( dist > max_dist ) max_dist = dist;
+        }
 
-  while (true) {
+        //-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
+        //-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
+        //-- small)
+        //-- PS.- radiusMatch can also be used here.
+        std::vector< DMatch > good_matches;
 
-  Mat img_2;
+        for( int i = 0; i < descriptors_1.rows; i++ )
+        { 
+            if( matches[i].distance <= max(2*min_dist, 0.02) )
+            { 
+                good_matches.push_back( matches[i]); 
+            }
+        }
 
-  img_2 = cvQueryFrame(cap);
+        //-- Draw only "good" matches
+        Mat img_matches;
+        drawMatches( img_1, keypoints_1, img_2, keypoints_2,
+                good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+                vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
 
-  detector.detect( img_2, keypoints_2 );
-  extractor.compute( img_2, keypoints_2, descriptors_2 );
+        //-- Show detected matches
+        imshow( "Roomba Detection", img_matches);
 
-  //-- Step 3: Matching descriptor vectors using FLANN matcher
-  FlannBasedMatcher matcher;
-  std::vector< DMatch > matches;
-  matcher.match( descriptors_1, descriptors_2, matches );
+        for( int i = 0; i < (int)good_matches.size(); i++ ) {
+            cout << "found match " << endl; 
+        }
 
-  double max_dist = 0; double min_dist = 100;
-
-  //-- Quick calculation of max and min distances between keypoints
-  for( int i = 0; i < descriptors_1.rows; i++ )
-  { double dist = matches[i].distance;
-    if( dist < min_dist ) min_dist = dist;
-    if( dist > max_dist ) max_dist = dist;
-  }
-
-  //-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
-  //-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
-  //-- small)
-  //-- PS.- radiusMatch can also be used here.
-  std::vector< DMatch > good_matches;
-
-  for( int i = 0; i < descriptors_1.rows; i++ )
-  { if( matches[i].distance <= max(2*min_dist, 0.02) )
-    { good_matches.push_back( matches[i]); }
-  }
-
-  //-- Draw only "good" matches
-  Mat img_matches;
-  drawMatches( img_1, keypoints_1, img_2, keypoints_2,
-               good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
-               vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
-
-  //-- Show detected matches
-  imshow( "Roomba Detection", img_matches);
-
-  for( int i = 0; i < (int)good_matches.size(); i++ ) {
-
-      cout << "found match " << endl; 
-  }
-
-  }
-
-  waitKey(0);
-
-
-
-
+        if(waitKey(10) >= 0)
+            break;
+    }
 }
